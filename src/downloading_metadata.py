2ba39b464e90b479a9898b1c53d8c0d194883fcd
alene <alene.rhea@gmail.com>
2020-04-02 21:10:28 -0400

    Adding index to metadata files

--- src/downloading_metadata.py
+++ src/downloading_metadata.py
@@ -5,8 +5,10 @@ from data_retrieval import open_url
 def save_json(url, index):
     html = open_url(url)
     entry = json.loads(html.replace('\\', ''), strict=False) #remove json escape characters
+    entry['index'] = index #add index to json
     file_name = index+'.json'
     file_path = pathlib.Path.cwd().parent / 'datasets' / 'downloaded_entries'/ 'metadata' /file_name
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(entry, f, ensure_ascii=False)               
     return 
+
